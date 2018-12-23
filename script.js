bed2ce8bc52ccbdcc0aad30de92a872f340690b6
Qhub <qdaman90@gmail.com>
2018-12-22 22:33:55 -0600

    Added my portfolio site

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+})
+
